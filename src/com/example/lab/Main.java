3dde9d20de23bee3ae22930a4c76edcac3ab1b93
wanilly <wanimagic@gmail.com>
2021-09-04 21:18:57 +0900

    lst commit

---
+++ src/com/example/lab/Main.java
@@ -0,0 +1,15 @@
+package com.example.lab;
+
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		String name;
+		System.out.print("What your name? > ");
+		name = s.next();
+		
+		System.out.print("Your name is "+name+".");
+	}
+}
